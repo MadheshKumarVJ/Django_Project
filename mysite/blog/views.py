a8bd5ee96101795966a58c7917a49f1ed7f2cda1
MadheshKumarVJ <vjmadhesh@testpress.in>
2022-10-19 15:55:06 +0530

    Add Ranking To post

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,6 +8,7 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Avg, Count
+from django.contrib.postgres.search import SearchVector, SearchQuery,SearchRank
 
 
 class PostListView(ListView):
@@ -89,7 +90,10 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(search=search_vector,
+                        rank=SearchRank(search_vector, search_query)).filter(search=search_query).order_by('-rank')
     return render(request, 'blog/post/search.html',
                   {'form': form,
                    'query': query,
